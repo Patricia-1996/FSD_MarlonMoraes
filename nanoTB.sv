954b52c36913131eee0f5eeaaa6a040227aa7d6d
Patricia-1996 <patricia.escobar@edu.pucrs.br>
2025-06-21 01:35:53 -0300

    Update nanoTB.sv

--- nanoTB.sv
+++ nanoTB.sv
@@ -32,7 +32,7 @@ module nanoCPU_TB;
      7: 'h6221,     //ADD R2 = R1 + 0 -> b = a
      8: 'h6113, // ADD R1= R3 +0 -> a= c
      9: 'h8000, //INC R0-> i++
-     10: 'h7420, //LESS R2  = i< 10
+     10: 'h7420, //LESS R2  = i< limit
      11: 'h30A5, // BRANCH to 5 if R2 == 1
      12: 'hF000, //END 
 
